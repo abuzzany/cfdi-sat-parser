1fc4d241526a0e6f0eb9a55e4b0016da1edd6e2d
Angel Buzany <angelbuzany@gmail.com>
2018-04-25 17:35:43 -0500

    Primer commit 	# Please enter the commit message for your changes. Lines starting

---
+++ lib/cfdi/sat/parser.rb
@@ -0,0 +1,9 @@
+require "cfdi/sat/parser/version"
+
+module Cfdi
+  module Sat
+    module Parser
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cfdi/sat/parser/version.rb
@@ -0,0 +1,7 @@
+module Cfdi
+  module Sat
+    module Parser
+      VERSION = "0.1.0"
+    end
+  end
+end
