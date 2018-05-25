0fcd8a5adc1c290cda1c27288ded5e4e867c3c63
Angel Buzany <angelbuzany@gmail.com>
2018-05-25 17:15:51 -0500

    Agrega clase Node

--- lib/cfdi/sat/parser/node.rb
+++ lib/cfdi/sat/parser/node.rb
@@ -1,11 +1,10 @@
 require "cfdi/sat/parser/version"
-require "cfdi/sat/parser/node"
 
 module Cfdi
   module Sat
     # Parase el XML del cfdi a un Hash
     module Parser
-      def self.run(xml)
+      class Node
       end
     end
   end
