b5c3194bba65ce73daf772896a724c9d473fb16d
Angel Buzany <angelbuzany@gmail.com>
2018-05-25 16:39:49 -0500

    Quita prueba inecesaria

--- spec/cfdi/sat/parser_spec.rb
+++ spec/cfdi/sat/parser_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Cfdi::Sat::Parser do
   it "has a version number" do
     expect(Cfdi::Sat::Parser::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
