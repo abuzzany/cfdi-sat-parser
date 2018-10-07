ef2262c414b6571145d55eea66627ab8dfe56c08
Angel buzany <abuzzany@gmail.com>
2018-10-07 00:44:03 -0500

    Mueve specs a su carpeta correcta

--- spec/cfdi_sat/v33/parser_spec.rb
+++ spec/cfdi_sat/v33/parser_spec.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe CfdiSat::V33::Parser do
-  it 'has a version number' do
-    expect(CfdiSat::VERSION).not_to be nil
-  end
-
   describe '.run' do
     it 'should returns a hash' do
       xml = 'factura patito'
---
+++ spec/cfdi_sat/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe CfdiSat::VERSION do
+  it 'has a version number' do
+    expect(CfdiSat::VERSION).not_to be nil
+  end
+end
