44aaea1ecb9e1633249162f337e5cab34e26c931
Angel buzany <abuzzany@gmail.com>
2018-10-07 21:55:33 -0500

    Agrega parsing de retained y transferred taxes

--- lib/cfdi_sat/v33/line_items/tax_detail_xml.rb
+++ lib/cfdi_sat/v33/line_items/tax_detail_xml.rb
@@ -5,9 +5,29 @@ module CfdiSat
     module LineIntems
       # Esta clase mapea los atributos del nodo 'Concepto#Impuestos'
       # y los devuelve en forma de Hash.
-      class TaxDetailXml
+      class TaxDetailXml < CfdiSat::V33::Node
+        # Regresa el nodo 'Concepto#Impuestos#Retenciones' del CFDI.
+        def retained_taxes
+          @retained_taxes ||= @element.xpath(
+            'cfdi:Retenciones'
+          )&.xpath('cfdi:Retencion')&.map do |retained_tax|
+            RetainedTaxXmlV33.new(retained_tax)
+          end
+        end
+
+        # Regresa el nodo 'Concepto#Imopuestos#Traslados' del CFDI.
+        def transferred_taxes
+          @transferred_taxes ||= @element.xpath(
+            'cfdi:Traslados'
+          )&.xpath('cfdi:Traslado')&.map do |transferred_tax|
+            TransferredTaxXmlV33.new(transferred_tax)
+          end
+        end
+
         private
 
+        # Mapea los nodos hijos que se tiene que parsear en el nodo
+        # 'Concepto#Impuestos' y que regresan un conjunto de nodos.
         def grand_children
           %i[retained_taxes transferred_taxes]
         end
