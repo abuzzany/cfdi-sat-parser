7279ad2d2d7ab6fdbbad508c5a267e249b0ca47d
Angel buzany <abuzzany@gmail.com>
2018-10-07 22:25:42 -0500

    Agrega parser de TaxDetailXml

---
+++ lib/cfdi_sat/v33/tax_detail_xml.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module CfdiSat
+    module V33
+      module LineIntems
+        # Esta clase mapea los atributos del nodo 'Comprobante#Impuestos'
+        # y los devuelve en forma de Hash.
+        class TaxDetailXml < CfdiSat::V33::Node
+          # Regresa el nodo 'Comprobante#Impuestos#Retenciones' del CFDI.
+          def retained_taxes
+            @retained_taxes ||= @element.xpath(
+              'cfdi:Retenciones'
+            )&.xpath('cfdi:Retencion')&.map do |retained_tax|
+              RetainedTaxXml.new(retained_tax)
+            end
+          end
+  
+          # Regresa el nodo 'Comprobante#Imopuestos#Traslados' del CFDI.
+          def transferred_taxes
+            @transferred_taxes ||= @element.xpath(
+              'cfdi:Traslados'
+            )&.xpath('cfdi:Traslado')&.map do |transferred_tax|
+              TransferredTaxXml.new(transferred_tax)
+            end
+          end
+  
+          private
+  
+          # Mapea los nodos hijos que se tiene que parsear en el nodo
+          # 'Comprobante#Impuestos' y que regresan un conjunto de nodos.
+          def grand_children
+            %i[retained_taxes transferred_taxes]
+          end
+        end
+      end
+    end
+  end
+  
