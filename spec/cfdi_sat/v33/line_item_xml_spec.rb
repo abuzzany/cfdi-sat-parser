0e75b27ff11a5663dadd8abc20081b9ded403769
Angel buzany <abuzzany@gmail.com>
2018-10-07 21:06:07 -0500

    Rubocop

--- spec/cfdi_sat/v33/line_item_xml_spec.rb
+++ spec/cfdi_sat/v33/line_item_xml_spec.rb
@@ -21,14 +21,14 @@ RSpec.describe CfdiSat::V33::LineItemXml do
         result = line_item.parse
 
         expected_line_item = { quantity: '2.0',
-                                     unit_type: 'TONELADA',
-                                     description: 'ACERO',
-                                     identify_num: '00001',
-                                     unity_code: 'F51',
-                                     product_service_code: '01010101',
-                                     unit_price_cents: 150_000_000,
-                                     total_cents: 225_000_000,
-                                     discount_cents: nil }
+                               unit_type: 'TONELADA',
+                               description: 'ACERO',
+                               identify_num: '00001',
+                               unity_code: 'F51',
+                               product_service_code: '01010101',
+                               unit_price_cents: 150_000_000,
+                               total_cents: 225_000_000,
+                               discount_cents: nil }
 
         expect(result).to be_eql(expected_line_item)
       end
